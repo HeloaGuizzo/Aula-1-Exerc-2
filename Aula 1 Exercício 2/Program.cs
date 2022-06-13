1864d372d50d15496e0d114309e59d20795820fa
HeloaGuizzo <107003060+HeloaGuizzo@users.noreply.github.com>
2022-06-13 17:05:34 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 1 Exercício 2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Karen Sparck Jones";
+            string formacao = "Cientista da Computação ";
+            string contribuicao = " Ela foi uma das criadoras do conceito de “inverso da frequência em " +
+                "documentos”, a base do que hoje são os sistemas de busca e localização de conteúdo e" +
+                " pedra fundamental de companhias como o Google, por exemplo.";
+
+            Console.WriteLine($"Nome: {nome}\nFormação: {formacao}\nContribuição: {contribuicao}\n");
+        }
+    }
+}
